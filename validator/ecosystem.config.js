5a4fa9293ab53b9eace6e1db2762d4ff879f1fb6
Franco NG <me@phan.co>
2025-12-03 10:38:31 +0100

    Update ecosystem

--- validator/ecosystem.config.js
+++ validator/ecosystem.config.js
@@ -6,8 +6,7 @@ module.exports = {
       env: {
         PORT: 3000,
         DB_PORT: 5432,
-        SENDER_CHAIN_ID: 31337,
-        RECEIVER_CHAIN_ID: 31338,
+        SUPPORTED_CHAIN_IDS: "11155111,11155420",
         NAME: "Melchior-0",
         PRIVATE_KEY:
           "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
@@ -19,8 +18,7 @@ module.exports = {
       env: {
         PORT: 3001,
         DB_PORT: 5433,
-        SENDER_CHAIN_ID: 31337,
-        RECEIVER_CHAIN_ID: 31338,
+        SUPPORTED_CHAIN_IDS: "11155111,11155420",
         NAME: "Balthasar-1",
         PRIVATE_KEY:
           "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
@@ -32,8 +30,7 @@ module.exports = {
       env: {
         PORT: 3002,
         DB_PORT: 5434,
-        SENDER_CHAIN_ID: 31337,
-        RECEIVER_CHAIN_ID: 31338,
+        SUPPORTED_CHAIN_IDS: "11155111,11155420",
         NAME: "Casper-2",
         PRIVATE_KEY:
           "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
